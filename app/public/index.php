c4e4da7cd2e50d83a00b574f869728251a92f2ba
gonzacap <gonzalo.october@gmail.com>
2024-04-28 23:38:16 -0300

    add update-persona post method

--- app/public/index.php
+++ app/public/index.php
@@ -1,6 +1,9 @@
 <?php
 require '../vendor/autoload.php';
 
+use App\Src\Models\Credenciales;
+use App\Src\Models\Persona;
+
 // Prepare app
 $app = new \Slim\Slim(array(
     'templates.path' => '../templates',
@@ -13,19 +16,34 @@ $app = new \Slim\Slim(array(
 ));
 
 // Prepare view
-$app->view(new \Slim\Views\Twig());
-$app->view->parserOptions = array(
-    'charset' => 'utf-8',
-    'cache' => realpath('../templates/cache'),
-    'auto_reload' => true,
-    'strict_variables' => false,
-    'autoescape' => true
-);
-$app->view->parserExtensions = array(new \Slim\Views\TwigExtension());
+// $app->view(new \Slim\Views\Twig());
+// $app->view->parserOptions = array(
+//     'charset' => 'utf-8',
+//     'cache' => realpath('../templates/cache'),
+//     'auto_reload' => true,
+//     'strict_variables' => false,
+//     'autoescape' => true
+// );
+// $app->view->parserExtensions = array(new \Slim\Views\TwigExtension());
 
 // Define routes
 $app->get('/', function () use ($app) {
     $app->render('index.html');
+});
+
+$app->post('/update-persona/{id}/{brand}', function ($request, $response, $args) {
+
+    $id = $args['id'];
+    $brand = $args['brand'];
+
+    $credenciales = Credenciales::where('brand', $brand)->first();
+
+    if (empty($credenciales)) {
+        return $response->withJson([
+            'estado' => 0,
+            'mensaje' => 'No se encontraron las credenciales para esta brand'
+        ], 404);
+    }
 });
 
 // Run app
